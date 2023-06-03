a8f9a6ea1b522557eae56e80dbd516c98c5b525b
xolabangaza <you@example.com>
2023-06-03 12:47:45 +0200

    some changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 // }
 const myText = document.getElementById("myText")
 const myList = document.getElementById("myList")
-
+// function for my add button
 function add() {
     if(myText.value=== ''){
         alert("Write something");
@@ -21,20 +21,25 @@ function add() {
     }
     myText.value = "";
     saveData();
+
+    let myDate = new Date(); //Get only the date console.log("date: ", 
+    console.log('date', myDate.toLocaleDateString()); 
+    //Obtain only the time
+     console.log("time: ", myDate.toLocaleTimeString());
 }
 
 
-myList.addEventListener("click",function(e){
-    if(e.target.tagName === "List"){
-         e.target.classList. toggle("checked");
-         saveData()
-    }
-    else if(e.target.tagName ==="SPAN"){
-        e.target.parentElement.remove();
-        saveData()
-    }
+// myList.addEventListener("click",function(e){
+//     if(e.target.tagName === "List") {
+//          e.target.classList. toggle("checked");
+//          saveData()
+//     }
+//     else if(e.target.tagName ==="SPAN"){
+//         e.target.parentElement.remove();
+//         saveData()
+//     }
 
-} , false);
+// } , false);
 
 
 function saveData() {
@@ -45,3 +50,46 @@ function showList(){
 
 }
 showList(); 
+
+
+function sorting() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("myList");
+    switching = true;
+
+    /* Make a loop that will continue until
+    no switching has been done: */
+
+    while (switching) {
+
+      // start by saying: no switching is done:
+
+      switching = false;
+      b = list.getElementsByTagName("LI");
+
+      // Loop through all list-items:
+      for (i = 0; i < (b.length - 1); i++) {
+
+        // start by saying there should be no switching:
+        shouldSwitch = false;
+
+        /* check if the next item should
+        switch place with the current item: */
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+
+          /* if next item is alphabetically
+          lower than current item, mark as a switch
+          and break the loop: */
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /* If switch is marked, make the switch
+        and mark the switch as done */
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+      }
+    }
+  }
+
